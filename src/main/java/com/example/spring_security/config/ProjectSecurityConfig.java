fc51da3ef05aa10169603c8241620d24ced5c529
HungHayHo <nguyenphuocnhahungl97@gmail.com>
2026-02-26 09:28:00 +0700

    config user by InMemoryUSerDetailManager

--- src/main/java/com/example/spring_security/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/spring_security/config/ProjectSecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -40,5 +44,12 @@ public class ProjectSecurityConfig {
                 withDefaults()
         );
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("user").password("{noop}123").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}321").authorities("admin").build();
+        return new InMemoryUserDetailsManager(user,admin);
     }
 }
