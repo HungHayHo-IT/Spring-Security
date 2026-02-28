f3402cbb84b2c58a37dac0e69a657bbe92759f02
HungHayHo <nguyenphuocnhahungl97@gmail.com>
2026-02-28 10:43:13 +0700

    tuy chinh xac thuc rieng

---
+++ src/main/java/com/example/spring_security/config/EazyBankUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.example.spring_security.config;
+
+import com.example.spring_security.model.Customer;
+import com.example.spring_security.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService; // Import thêm interface này
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EazyBankUserDetailsService implements UserDetailsService { // Thêm implements UserDetailsService
+
+    // Không cần @Autowired vì đã có @RequiredArgsConstructor của Lombok
+    private final CustomerRepository customerRepository;
+
+    @Override // Khai báo chuẩn phương thức của interface
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException { // Viết thường chữ 'l' và đổi throws
+        Customer customer = customerRepository.findByEmail(username).orElseThrow(
+                ()->new UsernameNotFoundException("User not found: " + username)
+        );
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(customer.getRole()));
+        return new User(customer.getEmail() , customer.getPwd() , authorities);
+    }
+}
